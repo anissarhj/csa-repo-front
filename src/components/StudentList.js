e2a5a0c3fe131b675213708b4c024ff7e81034a9
Anissa Ras <anissa.rasoanirinahj@gmail.com>
2024-02-01 13:32:37 +0100

    first commit front react

---
+++ src/components/StudentList.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const StudentList = () => {
+    const [students, setStudents] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:8080/api/students')
+            .then(response => {
+                setStudents(response.data);
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
+    }, []);
+
+    return (
+        <div>
+            <h1>Liste des étudiants</h1>
+            <ul>
+                {students.map(student => (
+                    <li key={student.id}> {student.nom}  {student.prenom}</li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default StudentList;
