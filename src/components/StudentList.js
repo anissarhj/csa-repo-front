d441325ca961123f7c3a232d1ece7c841d25427e
Anissa Ras <anissa.rasoanirinahj@gmail.com>
2024-02-05 10:45:31 +0100

    add config back

--- src/components/StudentList.js
+++ src/components/StudentList.js
@@ -5,7 +5,7 @@ const StudentList = () => {
     const [students, setStudents] = useState([]);
 
     useEffect(() => {
-        axios.get('http://localhost:8080/api/students')
+        axios.get('http://localhost:8081/api/students')
             .then(response => {
                 setStudents(response.data);
             })
